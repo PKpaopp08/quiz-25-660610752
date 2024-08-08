9fcc9f0f765d09e251999dfea8ae4d8b2fc20d1f
PAO <natthachai_bubpa@cmu.ac.th>
2024-08-08 15:31:36 +0700

    000000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-
+import PostOwnner from "PostOwnner";
 export default function Home() {
   return (
     <div
@@ -87,6 +86,41 @@ export default function Home() {
             </div>
           </div>
         </div>
+         <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src="/profileImages/puppy.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+                    <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/lisa.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Lisa
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>บ้าไปแล้ว</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+            </div>
+          </div>
+        </div>
+            
 
         {/* map-loop render Comment component here */}
       </div>
